e20b1e27b264971384333505d441bca9c6aba352
art-83 <arthurfmedeiros77@gmail.com>
2025-07-23 01:55:35 -0300

    feat: 01, 04

---
+++ Leet-Code/leet-code.js
@@ -0,0 +1,23 @@
+// 01. Two Sum
+function twoSum(nums, target) {
+    for (var i = 0; i < nums.length - 1; i++) {
+        for (var j = i + 1; j < nums.length; j++) {
+            if (nums[i] + nums[j] == target) {
+                return [i, j];
+            }
+        }
+    }
+    return [];
+}
+;
+// 04. Median of Two Sorted Arrays
+function findMedianSortedArrays(nums1, nums2) {
+    var nums3 = nums1.concat(nums2);
+    nums3.sort;
+    if (nums3.length % 2 != 0) {
+        return nums3[nums3.length / 2];
+    }
+    var sum = nums3[nums3.length / 2] + (nums3[nums3.length / 2] - 1);
+    return sum / 2;
+}
+;
